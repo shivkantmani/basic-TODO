a3413500432ef2f383fbf0841d6c371b7db90338
Shiv Kant Mani <Shivkant4852@gmail.com>
2023-10-13 18:54:02 +0530

    modify and improve the EJS and CSS files.

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import express from "express";
 
+
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
+
+
+
 app.get("/", (req,res) =>{
-    res.render("index.ejs");
+    res.render("today.ejs");
 });
+app.get("/work", (req,res) =>{
+    res.render("work.ejs");
+});
+
 
 app.listen(port,(req,res) =>{
     console.log(`listeng on port ${port}..`);
